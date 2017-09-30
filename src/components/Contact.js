44f7ecd79de9e733c01cf1fd7f836fdd5cd80e89
Derek Kramer <derek.kramer88@gmail.com>
2017-09-29 18:37:23 -0600

    💻 Fixing token issue

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -29,6 +29,7 @@ export default class Contact extends Component {
             this.setState({ error: 'Please enter a valid email' });
         }else{
             jwt.sign(this.state, process.env.REACT_APP_TOKEN, (err, token) => {
+                console.log(token);
                 axios.post('https://derekportfolioapi.herokuapp.com/', {token})
                 .then(result => {
                     if(result.data.status === 'success'){
